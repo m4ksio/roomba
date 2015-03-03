942e6a936f61aaf2cf7b36e67ed8b6106cf6e1c9
m4ks <maksy@skipjaq.com>
2015-03-03 18:54:06 +0000

    Position test

---
+++ src/test/scala/io/m4ks/PositionTest.scala
@@ -0,0 +1,26 @@
+package io.m4ks
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Created by m4ks on 03/03/15.
+ */
+class PositionTest extends FlatSpec with Matchers {
+
+  "Position" should "increase Y for North direction" in {
+    Position(0, 0).inDirection(Direction.N) shouldBe Position(0, 1)
+  }
+
+  it should "decrease Y for South direction" in {
+    Position(0, 0).inDirection(Direction.S) shouldBe Position(0, -1)
+  }
+
+  it should "decrease X for West direction" in {
+    Position(0, 0).inDirection(Direction.W) shouldBe Position(-1, 0)
+  }
+
+  it should "increase X for East direction" in {
+    Position(0, 0).inDirection(Direction.E) shouldBe Position(1, 0)
+  }
+
+}
